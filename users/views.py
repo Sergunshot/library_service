12de6c7438c90ae04760817b41b6739d41222a41
Sergiy Yurov <urovsergej10@gmail.com>
2025-05-03 18:15:43 +0300

    add CreateUserView and ManageUserView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create new user"""
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage user profile"""
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        """Get user"""
+        return self.request.user
