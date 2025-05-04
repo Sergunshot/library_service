c470c412a86d9a213cf681c48780fc3bc284c6b9
Sergiy Yurov <urovsergej10@gmail.com>
2025-05-04 12:07:32 +0300

    create BorrowingCreateSerializer

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField, StringRelatedField
 
@@ -20,3 +22,23 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "is_active"
         )
         read_only_fields = ("is_active", )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        model = Borrowing
+        fields = ("id", "book", "user", "borrow_date", "expected_return_date", )
+
+    def validate_expected_return_date(self, data):
+        if data < datetime.today().date():
+            raise serializers.ValidationError("Expected return date must be at least today.")
+        return data
+
+    def validate_book(self, data):
+        if data.inventory < 1:
+            raise serializers.ValidationError("The book cannot be borrowed: inventory=0.")
+        return data
